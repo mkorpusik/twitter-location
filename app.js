8f4a8b503f2cd02a348a17060f0c49fc15bf2419
mkorpusik <mkorpusik@ubuntu.(none)>
2013-03-25 14:13:40 -0400

    renders map info, with total number of tweets and average sentiment for each lat and long

--- app.js
+++ app.js
@@ -114,7 +114,46 @@ app.get('/stream', loginRequired, function (req, res) {
   });
 })
 
-var tweets_dict = {}; // global dictionary of tweets mapped to list of location and sentiment
+var tweets_dict = {}; // global dictionary of tweets mapped to list of location & sentiment
+
+function getMapInfo() {
+  // populate dictionary of locations (lat/lon) mapped to # tweets & average sentiment
+  var map_dict = {}; 
+  var loc1 = 'Boston, MA';
+  var loc2 = 'San Francisco, CA';
+  var loc3 = 'New York, NY';
+  var num1 = 0; // number tweets for loc1
+  var num2 = 0;
+  var num3 = 0;
+  var sent1 = 0; // total sentiment for loc1
+  var sent2 = 0;
+  var sent3 = 0;
+
+  // loop through all tweets
+  for (var tweet in tweets_dict) {
+
+    // increment num tweets & update sentiment for given location
+    if (loc1 == tweets_dict[tweet][1]) {
+      num1 += 1;
+      sent1 += tweets_dict[tweet][0];
+    } 
+    else if (loc2 == tweets_dict[tweet][1]) {
+      num2 += 1;
+      sent2 += tweets_dict[tweet][0];
+    }
+    else if (loc3 == tweets_dict[tweet][1]) {
+      num3 += 1;
+      sent3 += tweets_dict[tweet][0];
+    }
+  }
+
+  // assign values to map_dict
+  map_dict['42.3583,-71.0603'] = [num1, sent1/num1];
+  map_dict['37.775,-122.4183'] = [num2, sent2/num2];
+  map_dict['40.7142,-74.0064'] = [num3, sent3/num3];
+
+  return map_dict;
+}
 
 function getSentiment(tweet, location, render_page, res) {
   // calculates the sentiment of the tweet
@@ -130,7 +169,8 @@ function getSentiment(tweet, location, render_page, res) {
   
       // if render_page is true (i.e. all tweets have been found), renders jade
       if (render_page) {
-        res.render('tweets', { tweets:tweets_dict, title: 'Tweets' })
+        var map_dict = getMapInfo();
+        res.render('tweets', { tweets:tweets_dict, map_info:map_dict, title: 'tweets' })
       }
     });
   }).on('error', function(e) {
